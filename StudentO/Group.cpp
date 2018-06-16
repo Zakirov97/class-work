bbe0785c4a5b764db22dce3da2d148a9ea871629
Dias <fire.kiborgik@gmail.com>
2018-06-16 14:25:51 +0600

    Student Op

---
+++ StudentO/Group.cpp
@@ -0,0 +1,109 @@
+#include "Group.h"
+
+
+void Group::deleteStudent(int student_id)
+{
+	st.erase(st.begin() + student_id);
+}
+
+void Group::addStudent(Student st)
+{
+	this->st.push_back(st);
+}
+
+void Group::setGroupName(string groupName)
+{
+	this->groupName = groupName;
+}
+
+void Group::addMarkToStudent(Student &s, int markk)
+{
+	s.setMark(markk);
+}
+
+Group::Group()
+{
+}
+
+Group::Group(string groupName)
+{
+	this->groupName = groupName;
+}
+
+Group::Group(string groupName, vector<Student> st)
+{
+	this->groupName = groupName;
+	this->st = st;
+}
+
+
+void Group::saveToFile()
+{
+	vector<string> str;
+	for (int i = 0; i < st.size(); i++)
+	{
+		string str2 = st[i].codeInfo();
+		str.push_back(str2);
+	}
+	fstream f("group1.txt", std::ios::out);
+	ostream_iterator<string> out(f);
+	copy(str.begin(), str.end(), out);
+	f.close();
+}
+
+void Group::readFromFile()
+{
+	int temp = 0;
+	ifstream f("group1.txt");
+	while (!f.eof())
+	{
+		string ffirstName, llastName, ggroupName, ggender, pphone, marks;
+
+		getline(f, ffirstName, ';');
+		getline(f, llastName, ';');
+		getline(f, ggroupName, ';');
+		getline(f, ggender, ';');
+		getline(f, pphone, ';');
+		getline(f, marks, ';');
+		vector<int> v;
+		if (ffirstName[0] == '\n')
+			ffirstName.erase(0, 1);
+		if (ffirstName.size() > 0)
+		{
+			for (int i = 0; i < marks.size(); i++)
+			{
+				string st;
+				st = marks[i];
+				v.push_back(stoi(st));
+			}
+			st.push_back(Student(ffirstName, llastName, ggroupName, stoi(ggender), temp, stoi(pphone), v));
+		}
+		temp++;
+	}
+	f.close();
+}
+
+ostream & operator<<(ostream & out, Group &gr)
+{
+	cout << "Название группы - " << gr.getGroupName() << endl << endl;
+	for (int i = 0; i < gr.getStudents().size(); i++)
+	{
+		out << gr.getStudents()[i].getFirstName() << ' ' << gr.getStudents()[i].getLastName() << '\n';
+		if (gr.getStudents()[i].getGender())
+			cout << "Мужской пол" << ' ';
+		else
+			cout << "Женский пол" << ' ';
+		cout << "\nТелефон " << gr.getStudents()[i].getPhone() << '\n' << "ID " << gr.getStudents()[i].getID() << endl;
+		cout << "Оценки: ";
+		if (gr.getStudents()[i].getMarks()[0] != 0)
+			for (int j = 0; j < gr.getStudents()[i].getMarks().size(); j++)
+				cout << gr.getStudents()[i].getMarks()[j] << ' ';
+		else
+			cout << "оценок нет";
+		cout << endl;
+		cout << endl;
+
+	}
+	return out;
+}
+
---
+++ StudentO/Student.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+class Student
+{
+	string firstName;
+	string lastName;
+	string group;
+	bool gender;
+	int ID;
+	int phone;
+	vector<int> mark;
+public:
+	void setFirstName(string firstName);
+	void setLastName(string lastName);
+	void setGroup(string group);
+	void setGender(bool gender);
+	void setID(int ID);
+	void setPhone(int phone);
+	void setMark(int mark);
+
+
+	string getFirstName()const { return firstName; };
+	string getLastName()const { return lastName; };
+	string getGroup()const { return group; };
+	bool getGender()const { return gender; };
+	int getID()const { return ID; };
+	int getPhone()const { return phone; };
+	vector<int> getMarks() const;
+	virtual string codeInfo()const;
+	Student();
+	Student(string firstName, string lastName, string group, bool gender, int phone);
+	Student(string firstName, string lastName, string group, bool gender,int ID, int phone);
+	Student(string firstName, string lastName, string group, bool gender, int ID, int phone, vector<int> marks);
+	Student(string firstName, string lastName, string group, bool gender, int phone, vector<int> marks);
+
+	friend ostream& operator<<(ostream &out, const Student &st);
+};
+
+
